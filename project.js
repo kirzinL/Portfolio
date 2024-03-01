38fa799f28546283c5cc586090624074b8c2212e
Kirz <158538648+kirzinL@users.noreply.github.com>
2024-03-01 13:17:09 +0800

    1st commit

---
+++ project.js
@@ -0,0 +1,67 @@
+let projects = [
+    {
+        name:"Calculator",
+        tags:"#html/css",
+        image: "project1.png",
+    },
+    {
+        name:"Music player",
+        tags:"#javascript",
+        image: "project2.png",
+    },
+    {
+        name:"project three",
+        tags:"#html/css",
+        image: "project3.png",
+    },
+    {
+        name:"Grocery List",
+        tags:"#javascript #WebApp",
+        image: "GroceryList.png",
+    },
+    {
+        name:"To Do List",
+        tags:"#javascript #WebApp",
+        image: "todolist.png",
+    },
+    {
+        name:"Cat Age to Human",
+        tags:"#javascript",
+        image: "catage.png",
+    },
+    {
+        name:"Game Dev",
+        tags:"#Godot",
+        image: "gamedev1.png",
+    },
+    {
+        name:"Game Dev2",
+        tags:"#Godot",
+        image: "gamedev2.png",
+    },
+    {
+        name:"Anime site",
+        tags:"#figma,#Webdesign",
+        image: "Design1.png",
+    },
+    {
+        name:"Web design ",
+        tags:"#figma,#Webdesign",
+        image: "Desing2.png",
+    },
+    {
+        name:"Web design ",
+        tags:"#figma,#Webdesign",
+        image: "design3.png",
+    },
+    {
+        name:"Mobile design ",
+        tags:"#figma,#Webdesign",
+        image: "design4.png",
+    },
+   
+   
+   
+    
+    
+]
